db93816cda7742952cf1ca20e59ce8a09b8f85a0
S. Fleming <sfleming@q.com>
2017-06-29 00:34:46 -0500

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+            Console.ReadLine();
+        }
+
+        static void Execute()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine(i + " FizzBuzz");
+
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " Buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+        }
+    }
+}
